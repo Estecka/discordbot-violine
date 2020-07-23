db3ad6df01a76ff5bf5b03f40466c54d76ff9749
Estecka <estecka@galacsys.com>
2020-07-23 18:17:12 +0200

    Made Gulp a class.

--- Gulp.js
+++ Gulp.js
@@ -5,13 +5,61 @@
  */
 
 class Gulp {
+
+	/**
+	 * 
+	 * @param {string} sentence 
+	 */
+	constructor(sentence){
+		this.current = null;
+		this.remaining = sentence;
+	}
+
+	/**
+	 * Extract the next argument (delimited by whitespaces).
+	 * @return {string?} The argument, trimmed of whitespaces. Null if there is no argument.
+	 */
+	ShiftArg(){
+		let args = Gulp.ShiftSentence(this.remaining);
+		this.current = args.current ? args.current : null;
+		this.remaining = args.remaining ? args.current : null;
+		return this.current;
+	}
+
+	/**
+	 * Extracts the next positive decimal integer.
+	 * @return {number} The int value, or NaN if no int was found.
+	 */
+	ShiftInt(){
+		if (!IsDigit(this.remaining[0]))
+			return Number.NaN;
+		
+		this.current = 0;
+		let i = 0;
+		while (i < this.remaining.length && Gulp.IsDigit(this.remaining[i]))
+			this.current = (10 * value) + (this.remaining[i] - '0');
+
+		this.remaining = this.remaining.substr(i);
+
+		return this.current;
+	}
+
 	/**
 	 * Checks whether a given character is a whitespace
 	 * @param {character} char 
-	 * @returns {boolean} True if `char` is a whitespace
+	 * @returns {boolean} true if `char` is a whitespace
 	 */
 	static IsWhitespace  (char) {
 		return " \t\n\r\v\f".indexOf(char) >= 0;
+	}
+
+	/**
+	 * Checks whether a given character is a decimal character.
+	 * @param {character} char 
+	 * @return {boolean} true if `char` is a digit
+	 */
+	static IsDigit(char) {
+		return "0123456789".indexOf(char) >= 0;
 	}
 
 	/**
