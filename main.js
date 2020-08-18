8b14c57fec67fd72444e7080dc7d2df636877669
Estecka <estecka@galacsys.com>
2020-08-18 22:55:24 +0200

    Replaced auth.json with CLIENT_TOKEN env variable.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // Cause any uncaught error to be displayed on the console window for 5mn.
 process.on("uncaughtException", function(err){
-	console.log("\n/!\\ FATAL ERROR /!\\\n");
-	console.log(err);
+	console.error("\n/!\\ FATAL ERROR /!\\\n");
+	console.error(err);
 	setTimeout(()=>{process.exit()}, 300000);
 });
+
+require("dotenv").config();
+
+if (process.env.DISCORD_TOKEN == undefined)
+	throw "Environnement variable `DISCORD_TOKEN` is undefined.";
 
 // Adds the root path to the module listing
 module.paths.push("/");
@@ -11,11 +16,31 @@ module.paths.push("/");
 
 const http = require('http');
 const Discord = require('discord.js');
-var auth = require('./auth.json');
 
-// Get token from environnement variables if applicable.
-if (process.env.token != undefined)
-	auth.token = process.env.token;
+// Initialize Discord Client
+var client = new Discord.Client();
+
+const Violine = require('./violine.js');
+
+client.on('ready', function () {
+	console.log("Connected");
+	console.log("Logged in as : "  +client.user.username+"#"+client.user.discriminator +" ("+client.user.id+")");
+
+	Violine.Init(client);
+});
+
+
+client.on('message', function (msg) {
+	// Ignore own messages
+	if (msg.author.id == client.user.id)
+		return;
+
+	Violine.ProcessMessage(msg);
+});
+
+client.login();
+
+
 // Listen to http port if required.
 if (process.env.PORT !== undefined){
 	let server = http.createServer((req, res) => {
@@ -25,29 +50,3 @@ if (process.env.PORT !== undefined){
 	});
 	server.listen(process.env.PORT);
 }
-
-
-// Initialize Discord Client
-var client = new Discord.Client();
-
-const Violine = require('./violine.js');
-
-// -- READY --
-client.on('ready', function () {
-	console.log("Connected");
-	console.log("Logged in as : "  +client.user.username+"#"+client.user.discriminator +" ("+client.user.id+")");
-
-	Violine.Init(client);
-});
-
-
-// -- MESSSAGE --
-client.on('message', function (msg) {
-	// Ignore own messages
-	if (msg.author.id == client.user.id)
-		return;
-
-	Violine.ProcessMessage(msg);
-});
-
-client.login(auth.token);
